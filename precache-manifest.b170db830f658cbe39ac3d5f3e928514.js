ebb59aa3e7195073acc0bcb6280bd4668a9fff7b
Srikanth Khandavally <srikanth@Srikanths-MacBook-Pro.local>
2020-10-19 06:44:34 -0500

    Updates

---
+++ precache-manifest.b170db830f658cbe39ac3d5f3e928514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4a65479fe86a44b8c1da2ad4367529",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ff027049894bde419067",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "db32b0e02edeca6333f9",
+    "url": "/portfolio/static/css/main.b3efca0b.chunk.css"
+  },
+  {
+    "revision": "ff027049894bde419067",
+    "url": "/portfolio/static/js/2.5d52fb90.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/portfolio/static/js/2.5d52fb90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db32b0e02edeca6333f9",
+    "url": "/portfolio/static/js/main.8bf8ea37.chunk.js"
+  },
+  {
+    "revision": "272c84328c4e9a9d0bda",
+    "url": "/portfolio/static/js/runtime-main.6093c233.js"
+  }
+]);
