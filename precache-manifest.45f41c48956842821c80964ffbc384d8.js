e48d126103aa422cc337ddbe3b681e3ea7c852f3
Srikanth Khandavally <srikanth@Srikanths-MacBook-Pro.local>
2020-10-23 15:48:11 -0500

    Updates

--- precache-manifest.45f41c48956842821c80964ffbc384d8.js
+++ precache-manifest.45f41c48956842821c80964ffbc384d8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a38f5332c914cd69d99f161de9a534f",
+    "revision": "f30834bf63f1a440fb1365c0710613eb",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "60030cd9df78ec2a0b28",
+    "revision": "152581f0cd374fa1af4d",
     "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
   },
   {
-    "revision": "51ee9643f6c1a18bf6cd",
+    "revision": "868fbe21fcfb8b9d8e44",
     "url": "/portfolio/static/css/main.b3efca0b.chunk.css"
   },
   {
-    "revision": "60030cd9df78ec2a0b28",
-    "url": "/portfolio/static/js/2.5b17946f.chunk.js"
+    "revision": "152581f0cd374fa1af4d",
+    "url": "/portfolio/static/js/2.07968ec9.chunk.js"
   },
   {
     "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
-    "url": "/portfolio/static/js/2.5b17946f.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.07968ec9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "51ee9643f6c1a18bf6cd",
-    "url": "/portfolio/static/js/main.8b6dc9bd.chunk.js"
+    "revision": "868fbe21fcfb8b9d8e44",
+    "url": "/portfolio/static/js/main.2cee2a37.chunk.js"
   },
   {
     "revision": "272c84328c4e9a9d0bda",
@@ -34,6 +34,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d7e95c03ac416d338600744acb6bce9d",
     "url": "/portfolio/static/media/Sri-professional.d7e95c03.jpeg"
+  },
+  {
+    "revision": "e6466db84175b83c1b39cabcf026bc11",
+    "url": "/portfolio/static/media/bitmoji.e6466db8.JPG"
   },
   {
     "revision": "05ab50c3e979c96f1ca38c3bae73217d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5dba52213c2d435446ce6f894b2917b0.js"
+  "/portfolio/precache-manifest.45f41c48956842821c80964ffbc384d8.js"
 );
 
 self.addEventListener('message', (event) => {
